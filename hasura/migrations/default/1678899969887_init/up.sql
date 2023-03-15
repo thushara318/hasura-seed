775f4361b9d46f45ea42cd23fd96a7a1c81b3f95
thushara318 <thushara.s@perfomatix.com>
2023-03-15 22:48:50 +0530

    initial set up

--- hasura/migrations/default/1678899969887_init/up.sql
+++ hasura/migrations/default/1678899969887_init/up.sql
@@ -1,6 +1,6 @@
 SET check_function_bodies = false;
 CREATE TABLE public.test (
-    id uuid NOT NULL
+    id uuid DEFAULT gen_random_uuid() NOT NULL
 );
 COMMENT ON TABLE public.test IS 'test';
 ALTER TABLE ONLY public.test
